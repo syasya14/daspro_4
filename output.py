811e0b1dbdf95775df56ef1572a831034b49895e
syasya14 <sdevina790@gmail.com>
2025-03-09 16:26:26 +0700

    Add files via upload

---
+++ output.py
@@ -0,0 +1,9 @@
+angka = float(input("Masukan angka :"))
+
+if angka > 0:
+   print("Angka tersebut positif")
+elif angka < 0:
+   print("Angka tersebut adalah negatif")
+else:
+  print("Angka tersebut adalah nol")
+  
